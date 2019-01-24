16937adb02bf2813f9fe7989ea64a78b6fc70983
xinbin.cui <cuixinbin@yeah.net>
2019-01-24 18:10:32 +0800

    cross-domain spring session sharing
    cross-domain spring session sharing

---
+++ springboot-app1/src/main/java/sample/config/SessionSharingConfig.java
@@ -0,0 +1,20 @@
+package sample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CookieHttpSessionStrategy;
+import sample.config.SessionSharing.CuixbCookieSerializer;
+
+@Configuration
+@EnableRedisHttpSession
+public class SessionSharingConfig {
+
+    @Bean
+    public CookieHttpSessionStrategy cookieHttpSessionStrategy() {
+        CookieHttpSessionStrategy strategy = new CookieHttpSessionStrategy();
+        strategy.setCookieSerializer(new CuixbCookieSerializer());
+        return strategy;
+    }
+
+}
